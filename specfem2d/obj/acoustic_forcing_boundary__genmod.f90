0c6215d18b9a48262c936e8031909b99bf1fa2f8
Wenyong Pan <68055397+PanIGGCAS@users.noreply.github.com>
2020-07-27 12:02:53 +0800

    Add files via upload

---
+++ specfem2d/obj/acoustic_forcing_boundary__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Fri May 22 16:58:24 2020
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE ACOUSTIC_FORCING_BOUNDARY__genmod
+          INTERFACE 
+            SUBROUTINE ACOUSTIC_FORCING_BOUNDARY(IGLOB)
+              INTEGER(KIND=4), INTENT(IN) :: IGLOB
+            END SUBROUTINE ACOUSTIC_FORCING_BOUNDARY
+          END INTERFACE 
+        END MODULE ACOUSTIC_FORCING_BOUNDARY__genmod
