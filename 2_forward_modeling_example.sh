daaba8ab14c3ed88808deff0e387578e68f4d199
Wenyong Pan <68055397+PanIGGCAS@users.noreply.github.com>
2020-07-29 12:55:44 +0800

    Update 2_forward_modeling_example.sh

--- 2_forward_modeling_example.sh
+++ 2_forward_modeling_example.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd $SeisElastic2D/EXAMPLES/Model_generation/CW_VTI_syn
+cd $SeisElastic2D/EXAMPLES/Model_generation
+
+tar -zxvf CW_VTI_syn.tar.gz
+
+cd ./CW_VTI_syn
 
 bash run_this_example.sh
