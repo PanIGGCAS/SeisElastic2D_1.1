3e5328fec3b14b69ed72c1d1ed984a806f3d5ca3
Wenyong Pan <68055397+PanIGGCAS@users.noreply.github.com>
2020-07-29 12:40:28 +0800

    Update 4_plot_results.sh

--- 4_plot_results.sh
+++ 4_plot_results.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+## plot the true and initial models of elastic constants in VTI-elastic media
+
+python3 $VISUALIZE/plot_bin_kernel
+
+## plot the inverted elastic constants in VTI-elastic media
+
+## plot the inverted elastic constants in VTI-elastic media
